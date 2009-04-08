73b3aa29d72201a6ef4e9b08c9d21e8ad1f9a903
advansg <advansg@9039904a-1813-11de-ac9f-1d9250355bca>
2009-04-08 21:20:14 +0000

    + headers

--- branches/1.5.10/language/ja-JP/ja-JP.ignore.php
+++ branches/1.5.10/language/ja-JP/ja-JP.ignore.php
@@ -1,9 +1,23 @@
 <?php
-# $Id$
-# Joomla! Project
-# Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
-# License http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL, see LICENSE.php
-# Note : All ini files need to be saved as UTF-8 - No BOM
+/**
+ *
+ * @version	$Id$
+ * @package	Joomla
+ * @copyright Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
+ * @copyright Copyright (C) Translation 2007- 2008 [Translation Team url or Translator's name].
+ * @copyright Copyright (C) Translation 2008- 2009 [Translation Team url or Translator's name].
+ * @copyright Copyright (C) Translation 2009- 2010 [Translation Team url or Translator's name].
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+$search_ignore[] = "and";
+$search_ignore[] = "in";
+$search_ignore[] = "on";
 
 $search_ignore[]= "on";
 
