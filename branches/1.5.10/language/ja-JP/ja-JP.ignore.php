a18622257513b9195e9a19df2b9a678bcfa27ec8
taka.55@gmail.com <taka.55@gmail.com@9039904a-1813-11de-ac9f-1d9250355bca>
2009-04-10 18:37:14 +0000

    ^ copyright

--- branches/1.5.10/language/ja-JP/ja-JP.ignore.php
+++ branches/1.5.10/language/ja-JP/ja-JP.ignore.php
@@ -4,9 +4,8 @@
  * @version	$Id$
  * @package	Joomla
  * @copyright Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
- * @copyright Copyright (C) Translation 2007- 2008 [Translation Team url or Translator's name].
- * @copyright Copyright (C) Translation 2008- 2009 [Translation Team url or Translator's name].
- * @copyright Copyright (C) Translation 2009- 2010 [Translation Team url or Translator's name].
+ * @copyright Copyright (C) Translation 2007- 2008 Translation Team(http://www.joomla.jp).
+ * @copyright Copyright (C) Translation 2008- 2009 Translation Team(http://www.joomla.jp).
  * @license		GNU/GPL, see LICENSE.php
  * Joomla! is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
