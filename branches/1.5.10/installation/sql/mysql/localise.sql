ba760e61a4939e6de42f2a086c2dd6a8a6867b3d
phatstyle <phatstyle@9039904a-1813-11de-ac9f-1d9250355bca>
2009-04-14 07:47:18 +0000

    ^ localise.sql

--- branches/1.5.10/installation/sql/mysql/localise.sql
+++ branches/1.5.10/installation/sql/mysql/localise.sql
@@ -51,4 +51,4 @@ REPLACE `#__modules` VALUES (14, 'ユーザステータス','', 1,'status', 0,'0
 REPLACE `#__modules` VALUES (15, 'タイトル','', 1,'title', 0,'0000-00-00 00:00:00', 1,'mod_title', 0, 2, 1, '', 0, 1, '');
 
 REPLACE `#__menu_types` (`id`, `menutype`, `title`, `description`) VALUES
-(1, 'メインメニュー', 'Main Menu', 'このサイトのメインメニュー');
+(1, 'Main Menu', 'メインメニュー', 'このサイトのメインメニュー');
