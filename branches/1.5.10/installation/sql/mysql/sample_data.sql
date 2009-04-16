c0171341df7b0c74d9c9a22ee2bbf58401508e18
phatstyle <phatstyle@9039904a-1813-11de-ac9f-1d9250355bca>
2009-04-16 00:25:57 +0000

    ^ administrator/help/ja-JP/screen.weblink.html administrator/language/ja-JP/ja-JP.com_content.ini installation/sql/mysql/sample_data.sql language/ja-JP/ja-JP.mod_stats.ini language/ja-JP/ja-JP.mod_whosonline.ini

--- branches/1.5.10/installation/sql/mysql/sample_data.sql
+++ branches/1.5.10/installation/sql/mysql/sample_data.sql
@@ -374,7 +374,7 @@ REPLACE INTO `#__modules` (`id`, `title`, `content`, `ordering`, `position`, `ch
 (18, 'ログインフォーム', '', 8, 'left', 0, '0000-00-00 00:00:00', 1, 'mod_login', 0, 0, 1, 'greeting=1\nname=0', 1, 0, ''),
 (19, '最新ニュース', '', 4, 'user1', 0, '0000-00-00 00:00:00', 1, 'mod_latestnews', 0, 0, 1, 'cache=1', 1, 0, ''),
 (20, '統計', '', 6, 'left', 0, '0000-00-00 00:00:00', 0, 'mod_stats', 0, 0, 1, 'serverinfo=1\nsiteinfo=1\ncounter=1\nincrease=0\nmoduleclass_sfx=', 0, 0, ''),
-(21, 'Who''s Online', '', 1, 'right', 0, '0000-00-00 00:00:00', 1, 'mod_whosonline', 0, 0, 1, 'online=1\nusers=1\nmoduleclass_sfx=', 0, 0, ''),
+(21, 'オンライン中のユーザ', '', 1, 'right', 0, '0000-00-00 00:00:00', 1, 'mod_whosonline', 0, 0, 1, 'online=1\nusers=1\nmoduleclass_sfx=', 0, 0, ''),
 (22, 'ポピュラー', '', 6, 'user2', 0, '0000-00-00 00:00:00', 1, 'mod_mostread', 0, 0, 1, 'cache=1', 0, 0, ''),
 (23, 'アーカイブ', '', 9, 'left', 0, '0000-00-00 00:00:00', 0, 'mod_archive', 0, 0, 1, 'cache=1', 1, 0, ''),
 (24, 'セクション', '', 10, 'left', 0, '0000-00-00 00:00:00', 0, 'mod_sections', 0, 0, 1, 'cache=1', 1, 0, ''),
