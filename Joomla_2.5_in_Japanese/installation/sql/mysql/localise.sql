c751899b24fcae1354744e2be4084351265cdf7b
phatstyle@gmail.com <phatstyle@gmail.com@9039904a-1813-11de-ac9f-1d9250355bca>
2012-04-05 05:32:02 +0000

    エクステンション番号変更

--- Joomla_2.5_in_Japanese/installation/sql/mysql/localise.sql
+++ Joomla_2.5_in_Japanese/installation/sql/mysql/localise.sql
@@ -13,8 +13,8 @@ REPLACE INTO `#__extensions` VALUES(11, 'com_languages', 'component', 'com_langu
 # Languages
 
 INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `custom_data`, `system_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
-(600, '日本語', 'language', 'ja-JP', '', 0, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0),
-(601, '日本語', 'language', 'ja-JP', '', 1, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0);
+(602, '日本語', 'language', 'ja-JP', '', 0, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0),
+(603, '日本語', 'language', 'ja-JP', '', 1, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0);
 
 REPLACE INTO `#__languages` (`lang_id`,`lang_code`,`title`,`title_native`,`sef`,`image`,`description`,`metakey`,`metadesc`, `published`, `ordering`) VALUES
 (1, 'en-GB', 'English (UK)', 'English (UK)', 'en', 'en', '', '', '', 1, 2),
